cfeee05ad339730c27f3533cf18a80cfdea77ca2
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2020-08-27 15:35:47 +0200

    Bouton annuler les changements

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const img = document.getElementById('img');
 const filterControls = document.querySelectorAll('input[type=range]');
+const cancelButton = document.getElementById('cancelButton');
+const changeImageButton = document.getElementById('changeImageButton');
 
 filterControls.forEach(function(item, index) {
-	item.addEventListener('input', () => {
-		applyFilters();
-	})
+    item.addEventListener('input', () => {
+        applyFilters();
+    });
 });
 
-function applyFilters() {
-	let computedFilters = '';
-	filterControls.forEach(function(item, index) {
-		computedFilters += item.getAttribute('data-filter') + '(' + item.value + item.getAttribute('data-scale') + ') ';
-		let span = document.getElementById(item.getAttribute('data-filter'));
-		span.innerText = item.value + ((item.getAttribute('data-scale') == 'deg') ? '°' : '%');
-	});
-	img.style.filter = computedFilters;
+function applyFilters(apply = true) {
+    let computedFilters = '';
+    filterControls.forEach(function(item, index) {
+        if (!apply) item.value = item.getAttribute('data-default');
+        computedFilters += item.getAttribute('data-filter') + '(' + item.value + item.getAttribute('data-scale') + ') ';
+        let span = document.getElementById(item.getAttribute('data-filter'));
+        span.innerText = item.value + ((item.getAttribute('data-scale') == 'deg') ? '°' : '%');
+    });
+    img.style.filter = computedFilters;
 }
+
+cancelButton.addEventListener('click', () => {
+    applyFilters(false);
+});
